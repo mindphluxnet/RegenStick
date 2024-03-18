7c2e381e02792e0bbcd796d43c2b5ff6a3ac70df
el211 <86126606+el211@users.noreply.github.com>
2024-03-18 14:33:49 +0100

    first commit

---
+++ src/main/java/com/afelia/regenstick/RegenStick.java
@@ -0,0 +1,17 @@
+package com.afelia.regenstick;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RegenStick extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
