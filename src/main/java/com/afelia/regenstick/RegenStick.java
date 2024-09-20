07b282d5c22c116552fcb2cb1b6756b8587006bb
mindphlux <richard.kaemmerer@gmail.com>
2024-09-20 12:11:39 +0200

    Overhaul

--- src/main/java/com/afelia/regenstick/RegenStick.java
+++ src/main/java/com/afelia/regenstick/RegenStick.java
@@ -1,6 +1,7 @@
 package com.afelia.regenstick;
 
-import org.bukkit.Material;
+import net.shortninja.test.Vector3D;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -8,14 +9,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemRarity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
 
 
 import java.util.ArrayList;
@@ -24,8 +27,9 @@ import java.util.List;
 import java.util.Map;
 
 public class RegenStick extends JavaPlugin implements Listener {
-    private int maxUses;
     private int cooldownSeconds;
+    private List<String> stickLore = new ArrayList<>();
+    private String stickName;
     private Map<Player, Long> lastUsedTimeMap;
 
     @Override
@@ -38,13 +42,14 @@ public class RegenStick extends JavaPlugin implements Listener {
 
     private void loadConfig() {
         FileConfiguration config = getConfig();
-        maxUses = config.getInt("max_uses", 10);
         cooldownSeconds = config.getInt("cooldown", 5);
+        stickName = config.getString("stick_name");
+        stickLore = config.getStringList("stick_lore");
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (command.getName().equalsIgnoreCase("giveregenstick")) {
+        if (command.getName().equalsIgnoreCase("gethealingwand")) {
             if (!(sender instanceof Player)) {
                 sender.sendMessage("Only players can use this command!");
                 return true;
@@ -52,18 +57,20 @@ public class RegenStick extends JavaPlugin implements Listener {
             Player player = (Player) sender;
 
             // Check if player has permission to give regeneration sticks
-            if (!player.hasPermission("regenstick.give")) {
+            if (!player.hasPermission("healingwand.give")) {
                 player.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
                 return true;
             }
 
-            int remainingUses = 0; // Initially set to zero uses
-            ItemStack stick = createRegenerationStick(remainingUses);
-            player.getInventory().addItem(stick);
+            ItemStack stick = createRegenerationStick();
+            if(!player.getInventory().contains(stick)) {
+                player.getInventory().addItem(stick);
+            }
+
             return true;
-        } else if (command.getName().equalsIgnoreCase("giveplayerregenstick")) {
+        } else if (command.getName().equalsIgnoreCase("givehealingwand")) {
             if (args.length != 1) {
-                sender.sendMessage(ChatColor.RED + "Usage: /giveplayerregenstick <player>");
+                sender.sendMessage(ChatColor.RED + "Usage: /givehealingwand <player>");
                 return false;
             }
 
@@ -80,34 +87,37 @@ public class RegenStick extends JavaPlugin implements Listener {
             }
 
             // Check if console has permission to give regeneration sticks to other players
-            if (!sender.hasPermission("regenstick.giveplayer")) {
+            if (!sender.hasPermission("healingwand.giveplayer")) {
                 sender.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
                 return true;
             }
 
-            int remainingUses = 0; // Initially set to zero uses
-            ItemStack stick = createRegenerationStick(remainingUses);
-            target.getInventory().addItem(stick);
-            sender.sendMessage(ChatColor.GREEN + "You have given a regeneration stick to " + target.getName() + ".");
+            ItemStack stick = createRegenerationStick();
+            if(!target.getInventory().contains(stick)) {
+                target.getInventory().addItem(stick);
+                sender.sendMessage(ChatColor.GREEN + "You have given a healing wand to " + target.getName() + ".");
+            }
             return true;
         }
         return false;
     }
 
-
-
-
-    private ItemStack createRegenerationStick(int remainingUses) {
+    private ItemStack createRegenerationStick() {
         ItemStack stick = new ItemStack(Material.STICK);
         ItemMeta meta = stick.getItemMeta();
+        NamespacedKey key = new NamespacedKey(this, "healingwand");
+
         if (meta != null) {
             // Set display name with color codes
-            meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "Regeneration Stick"));
+            meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', stickName));
+            meta.getPersistentDataContainer().set(key, PersistentDataType.BOOLEAN, true);
 
             // Set lore with color codes
             List<String> lore = new ArrayList<>();
-            lore.add(ChatColor.translateAlternateColorCodes('&', "Right-click to regenerate health over time!"));
-            lore.add(ChatColor.translateAlternateColorCodes('&', "Remaining Uses: " + remainingUses + "/" + maxUses));
+            for (String s : stickLore) {
+                lore.add(ChatColor.translateAlternateColorCodes('&', s));
+            }
+
             meta.setLore(lore);
 
             // Retrieve custom model data from config
@@ -120,58 +130,126 @@ public class RegenStick extends JavaPlugin implements Listener {
         return stick;
     }
 
-
+    private boolean isHealingWand(ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        NamespacedKey key = new NamespacedKey(this, "healingwand");
+        PersistentDataContainer container = meta.getPersistentDataContainer();
+        if(meta.getPersistentDataContainer().has(key)) {
+            return container.get(key, PersistentDataType.BOOLEAN);
+        };
+        return false;
+    }
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
+        int healAmount = 4;
         Player player = event.getPlayer();
         ItemStack item = player.getInventory().getItemInMainHand();
 
         if (item != null && item.getType() == Material.STICK && item.hasItemMeta()) {
             ItemMeta meta = item.getItemMeta();
-            if (meta != null && meta.hasDisplayName() && meta.getDisplayName().equals("Regeneration Stick")) {
+            if (meta != null && isHealingWand(item)) {
                 if (checkCooldown(player)) {
-                    player.sendMessage("Stick is on cooldown!");
-                    return;
-                }
-                int uses = getRemainingUses(meta);
-                if (uses >= maxUses) {
-                    player.sendMessage("You've used up all the charges on this stick!");
+                    player.sendMessage("Wand is on cooldown!");
                     return;
                 }
 
-                double maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
-                double currentHealth = player.getHealth();
-                double newHealth = Math.min(maxHealth, currentHealth + 4); // Ensure health doesn't exceed max
-
-                player.setHealth(newHealth); // Regenerate 2 hearts (4 health points)
-
-                // Update the lore to reflect the remaining uses
-                List<String> lore = meta.getLore();
-                if (lore != null && !lore.isEmpty()) {
-                    lore.set(1, "Remaining Uses: " + (uses + 1) + "/" + maxUses);
-                    meta.setLore(lore);
-                    item.setItemMeta(meta);
-                    updateLastUsedTime(player);
+                Player targetPlayer = getTargetPlayer(player);
+                if (targetPlayer == null) {
+                    // self heal with no target
+                    targetPlayer = player;
                 }
+
+                double maxHealth = targetPlayer.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
+                double currentHealth = targetPlayer.getHealth();
+
+                // don't use charges if the target is at max health
+
+                if(currentHealth >= maxHealth) {
+                    if(targetPlayer == player) {
+                        player.sendMessage("You are already at full health!");
+                        return;
+                    }
+                    player.sendMessage("Target player is already at full health!");
+                    return;
+                }
+
+                if(targetPlayer != player) {
+                    drawParticles(player, targetPlayer);
+                }
+
+                double newHealth = Math.min(maxHealth, currentHealth + healAmount); // Ensure health doesn't exceed max
+
+                targetPlayer.setHealth(newHealth); // Regenerate 2 hearts (4 health points)
+
+
+                updateLastUsedTime(player);
+
             }
         }
     }
 
+    private void drawParticles(Player player, Player targetPlayer) {
+        // Draw heart particles between caster and target
+        Vector playerLocVector = player.getLocation().toVector();
+        Vector targetLocVector = targetPlayer.getLocation().toVector();
+        Vector betweenPandTVector = targetLocVector.clone().subtract(playerLocVector);
+        Vector directionVector = betweenPandTVector.clone().normalize();
 
-    private int getRemainingUses(ItemMeta meta) {
-        List<String> lore = meta.getLore();
-        if (lore != null && lore.size() >= 2) {
-            String remainingUsesLine = lore.get(1);
-            String[] parts = remainingUsesLine.split("/");
-            if (parts.length == 2) {
-                try {
-                    return Integer.parseInt(parts[0].replaceAll("\\D+", ""));
-                } catch (NumberFormatException ignored) {
+        for(int i = 0; i < betweenPandTVector.length(); i++) {
+            Vector particlePoint = playerLocVector.clone().add(directionVector.clone().multiply(i));
+            player.getWorld().spawnParticle(Particle.HEART,
+                    particlePoint.getX(),
+                    particlePoint.getY() + 1,
+                    particlePoint.getZ(),
+                    0, 0.001, 1, 0, 1,
+                    new Particle.DustOptions(Color.RED, 1));
+        }
+    }
+
+    private Player getTargetPlayer(Player player)
+    {
+        Player targetPlayer = null;
+        Location playerPos = player.getEyeLocation();
+        Vector3D playerDir = new Vector3D(playerPos.getDirection());
+        Vector3D playerStart = new Vector3D(playerPos);
+        Vector3D playerEnd = playerStart.add(playerDir.multiply(100));
+
+        for(Player p : player.getWorld().getPlayers())
+        {
+            Vector3D targetPos = new Vector3D(p.getLocation());
+            Vector3D minimum = targetPos.add(-0.5, 0, -0.5);
+            Vector3D maximum = targetPos.add(0.5, 1.67, 0.5);
+
+            if(p != player && hasIntersection(playerStart, playerEnd, minimum, maximum))
+            {
+                if(targetPlayer == null || targetPlayer.getLocation().distanceSquared(playerPos) > p.getLocation().distanceSquared(playerPos))
+                {
+                    targetPlayer = p;
                 }
             }
         }
-        return 0;
+
+        return targetPlayer;
+    }
+
+    private boolean hasIntersection(Vector3D p1, Vector3D p2, Vector3D min, Vector3D max)
+    {
+        final double epsilon = 0.0001f;
+        Vector3D d = p2.subtract(p1).multiply(0.5);
+        Vector3D e = max.subtract(min).multiply(0.5);
+        Vector3D c = p1.add(d).subtract(min.add(max).multiply(0.5));
+        Vector3D ad = d.abs();
+
+        if(Math.abs(c.x) > e.x + ad.x) return false;
+        if(Math.abs(c.y) > e.y + ad.y) return false;
+        if(Math.abs(c.z) > e.z + ad.z) return false;
+
+        if(Math.abs(d.y * c.z - d.z * c.y) > e.y * ad.z + e.z * ad.y + epsilon) return false;
+        if(Math.abs(d.z * c.x - d.x * c.z) > e.z * ad.x + e.x * ad.z + epsilon) return false;
+        if(Math.abs(d.x * c.y - d.y * c.x) > e.x * ad.y + e.y * ad.x + epsilon) return false;
+
+        return true;
     }
 
     private boolean checkCooldown(Player player) {
